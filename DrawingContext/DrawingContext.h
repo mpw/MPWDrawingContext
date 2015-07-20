d94247fffe2e4856d0c40bbec4ba090cdce175e5
Marcel Weiher <marcel@metaobject.com>
2015-07-20 17:14:43 +0200

    Initial Commit

---
+++ DrawingContext/DrawingContext.h
@@ -0,0 +1,19 @@
+//
+//  DrawingContext.h
+//  DrawingContext
+//
+//  Created by Marcel Weiher on 7/20/15.
+//  Copyright © 2015 metaobject. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for DrawingContext.
+FOUNDATION_EXPORT double DrawingContextVersionNumber;
+
+//! Project version string for DrawingContext.
+FOUNDATION_EXPORT const unsigned char DrawingContextVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DrawingContext/PublicHeader.h>
+
+
