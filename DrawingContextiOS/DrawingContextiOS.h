204652abf7abd248bd1b2525449166f3bb422555
Marcel Weiher <marcel@metaobject.com>
2015-07-24 11:01:56 +0200

    gitignore and the iOS dir (not really needed)

---
+++ DrawingContextiOS/DrawingContextiOS.h
@@ -0,0 +1,19 @@
+//
+//  DrawingContextiOS.h
+//  DrawingContextiOS
+//
+//  Created by Marcel Weiher on 7/23/15.
+//  Copyright © 2015 metaobject. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DrawingContextiOS.
+FOUNDATION_EXPORT double DrawingContextiOSVersionNumber;
+
+//! Project version string for DrawingContextiOS.
+FOUNDATION_EXPORT const unsigned char DrawingContextiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DrawingContextiOS/PublicHeader.h>
+
+
